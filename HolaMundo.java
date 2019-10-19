10267a4bf4031aadd00492d02a8f1505782842e3
easalazar14 <ericksalazaralex@gmail.com>
2019-10-18 19:53:17 -0500

    Primera Compilacion en Java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 // Mi primer programa en Java
-Public class HolaMundo {
-=======
 public class HolaMundo {
->>>>>>> 8b325af81f24cbed9e446de80b60fc86d9bf164e
   public static void main(String[] args){
-    System.out.println("Hola Mundo");
-  }
+    System.out.println("Hola Mundo c:");
+	}
 }
+
